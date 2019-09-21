3e2669a01c752c646232056aa98b328882c39a7b
Rafael Cadena <rafaeljcadena@gmail.com>
2019-09-21 17:02:22 -0300

    Updates

---
+++ precache-manifest.c98b7c94f807c1199085e91894e870e8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dba4a6b88d90602ad9d3a617161d0703",
+    "url": "/work-react-cadena/index.html"
+  },
+  {
+    "revision": "f6abc602952d4183ed6f",
+    "url": "/work-react-cadena/static/css/main.3c7e4103.chunk.css"
+  },
+  {
+    "revision": "38a6b9dd62301db2cce2",
+    "url": "/work-react-cadena/static/js/2.72921c67.chunk.js"
+  },
+  {
+    "revision": "f6abc602952d4183ed6f",
+    "url": "/work-react-cadena/static/js/main.3495cb4c.chunk.js"
+  },
+  {
+    "revision": "c6714519e1c650edea5c",
+    "url": "/work-react-cadena/static/js/runtime-main.0f730579.js"
+  }
+]);
